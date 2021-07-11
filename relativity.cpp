4d395a43685a7e9178fb533e39f07f0d8eef1321
abhishek-songirkar <72520447+abhishek-songirkar@users.noreply.github.com>
2021-07-11 12:23:33 +0530

    Create relativity.cpp

---
+++ relativity.cpp
@@ -0,0 +1,13 @@
+//cpp
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int g,c;
+        cin>>g>>c;
+        cout<<(c*c)/(2*g)<<endl;
+    }
+}
